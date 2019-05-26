2d14dcea83cf7fd1692e2ef9a9901b2d62926d9d
NadezhdaSukhotskaya <47493685+NadezhdaSukhotskaya@users.noreply.github.com>
2019-05-26 17:43:41 +0300

    add GetNameServlet

--- server/src/main/java/servlets/GetNameServlet.java
+++ server/src/main/java/servlets/GetNameServlet.java
@@ -1,4 +1,37 @@
 package servlets;
 
-public class GetNameServlet {
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/get")
+public class GetNameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+
+            String name = request.getParameter("name");
+
+            if (name != null) {
+                if (name.length() > 100) {
+                    response.getOutputStream().println("Too long name!");
+                } else {
+                    response.getOutputStream().println("Name is " + name);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Input/output error!");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        doGet(request, response);
+    }
 }
