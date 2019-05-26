ac60399caee00b932f5229c2e13eb9ddfc30b583
NadezhdaSukhotskaya <47493685+NadezhdaSukhotskaya@users.noreply.github.com>
2019-05-26 17:41:53 +0300

    add FilterTest

--- server/src/main/java/servlets/FilterTest.java
+++ server/src/main/java/servlets/FilterTest.java
@@ -1,4 +1,28 @@
 package servlets;
 
-public class TestFilter {
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/", "/status", "/get", "/check", "/page", "/test1", "/test2"})
+public class FilterTest implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest)request;
+        String method = req.getMethod();
+        String url = req.getRequestURL().toString();
+        Long workTime = System.currentTimeMillis();
+
+        chain.doFilter(request, response);
+        workTime = System.currentTimeMillis() - workTime;
+        System.out.println(method + " + " + url + " + " + workTime + "ms");
+    }
 }
