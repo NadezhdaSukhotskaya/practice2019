a1f90be85b24648704c9bdf89cb2097a1db85d44
NadezhdaSukhotskaya <47493685+NadezhdaSukhotskaya@users.noreply.github.com>
2019-05-26 17:45:46 +0300

    add ServletCheck

--- server/src/main/java/servlets/ServletCheck.java
+++ server/src/main/java/servlets/ServletCheck.java
@@ -1,4 +1,29 @@
 package servlets;
 
-public class ServletCheck {
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/check")
+public class ServletCheck extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Gson jsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+        String answer = jsonBuilder.toJson(new Answer("Moment", true));
+        response.getOutputStream().println(answer);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doPost(request, response);
+    }
 }
+
